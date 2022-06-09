f934e768b1f1f6032e18f2ee8060aaa7accaa2fa
SeungHun6450 <toffg6450@naver.com>
2022-06-09 21:44:45 +0900

    apitest

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <RouterView name="TheHeader" />
   <RouterView />
+  <APITest />
 </template>
 
 <script>
+import APITest from '~/routes/APITest.vue'
+
+export default {
+  components: {
+    APITest
+  }
+}
 </script>
 
 <style lang="scss">
---
+++ src/routes/APITest.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>About</h1>
+  <RouterLink to="/about/name">
+    Name~
+  </RouterLink>
+  <RouterView />
+
+  <input
+    type="file" 
+    @change="selectFile" />
+</template>
+
+<script>
+export default {
+  methods: {
+    selectFile(event) {
+      console.log(event)
+      const reader = new FileReader()
+      for (const file of event.target.files) {
+        console.log(file)
+        reader.readAsDataURL(file)
+        reader.addEventListener('load', e => {
+          console.log(e.target.result)  // base64의 값이 들어있다!
+        })
+      }
+    }
+  }
+}
+</script>
